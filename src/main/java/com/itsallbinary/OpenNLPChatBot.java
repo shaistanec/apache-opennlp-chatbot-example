bd43c75c6777b3806e7097abb94cdd25cdc9f6af
shaista.shafi <onr@kmd.dk>
2022-10-10 12:41:47 +0530

    initial commit

--- src/main/java/com/itsallbinary/OpenNLPChatBot.java
+++ src/main/java/com/itsallbinary/OpenNLPChatBot.java
@@ -1,8 +1,21 @@
 package com.itsallbinary;
 
+import opennlp.tools.doccat.*;
+import opennlp.tools.lemmatizer.LemmatizerME;
+import opennlp.tools.lemmatizer.LemmatizerModel;
+import opennlp.tools.namefind.*;
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSTaggerME;
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
+import opennlp.tools.util.*;
+import opennlp.tools.util.eval.FMeasure;
+import opennlp.tools.util.model.ModelUtil;
+
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -12,252 +25,159 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-import opennlp.tools.doccat.BagOfWordsFeatureGenerator;
-import opennlp.tools.doccat.DoccatFactory;
-import opennlp.tools.doccat.DoccatModel;
-import opennlp.tools.doccat.DocumentCategorizerME;
-import opennlp.tools.doccat.DocumentSample;
-import opennlp.tools.doccat.DocumentSampleStream;
-import opennlp.tools.doccat.FeatureGenerator;
-import opennlp.tools.lemmatizer.LemmatizerME;
-import opennlp.tools.lemmatizer.LemmatizerModel;
-import opennlp.tools.postag.POSModel;
-import opennlp.tools.postag.POSTaggerME;
-import opennlp.tools.sentdetect.SentenceDetectorME;
-import opennlp.tools.sentdetect.SentenceModel;
-import opennlp.tools.tokenize.TokenizerME;
-import opennlp.tools.tokenize.TokenizerModel;
-import opennlp.tools.util.InputStreamFactory;
-import opennlp.tools.util.InvalidFormatException;
-import opennlp.tools.util.MarkableFileInputStreamFactory;
-import opennlp.tools.util.ObjectStream;
-import opennlp.tools.util.PlainTextByLineStream;
-import opennlp.tools.util.TrainingParameters;
-import opennlp.tools.util.model.ModelUtil;
+import static opennlp.tools.ml.TrainerFactory.TrainerType.EVENT_MODEL_SEQUENCE_TRAINER;
+import static opennlp.tools.ml.TrainerFactory.TrainerType.SEQUENCE_TRAINER;
 
-/**
- * Custom chat bot or chat agent for automated chat replies for FAQs. It uses
- * different features of Apache OpenNLP for understanding what user is asking
- * for. NLP is natural language processing.
- * 
- * @author itsallbinary
- *
- */
 public class OpenNLPChatBot {
 
-	private static Map<String, String> questionAnswer = new HashMap<>();
+    private static Map<String, String> questionAnswer = new HashMap<>();
+    private static Map<String, Object> questionAnswers = new HashMap<>();
+    static {
+        questionAnswers.put("greeting", "Hello, how can I help you?");
+        questionAnswers.put("team-names", "Stratus, NynemKonto, Emerging Tech & Innovation");
+//        questionAnswers.put("stratus-enquiry", "Mamatha, Srinivas, Henrik, Mads, Shilpa");
+        questionAnswers.put("nynemkonto-enquiry", "Mamatha, Srinivas, Henrik, Mads, Shilpa, Shaista, Pranati, Nithya, Karthik, Shrinivasa");
+        questionAnswers.put("emergingTech-enquiry", "Mamatha, Shaista, Shrusti, Shilpa, Karthik, Shrinivasa");
+        questionAnswers.put("team-experience", "5");
+        questionAnswers.put("conversation-continue", "What else can I help you with?");
+        questionAnswers.put("conversation-complete", "Nice chatting with you. Bbye.");
 
-	/*
-	 * Define answers for each given category.
-	 */
-	static {
-		questionAnswer.put("greeting", "Hello, how can I help you?");
-		questionAnswer.put("product-inquiry",
-				"Product is a TipTop mobile phone. It is a smart phone with latest features like touch screen, blutooth etc.");
-		questionAnswer.put("price-inquiry", "Price is $300");
-		questionAnswer.put("conversation-continue", "What else can I help you with?");
-		questionAnswer.put("conversation-complete", "Nice chatting with you. Bbye.");
+    }
 
-	}
+    static {
+        questionAnswer.put("greeting", "Hello, how can I help you?");
+        questionAnswer.put("product-inquiry",
+                "Product is a Samsung mobile. It is a smart phone with latest features like touch screen, bluetooth etc.");
+        questionAnswer.put("price-inquiry", "Price is $3000");
+        questionAnswer.put("colours-enquiry", "Sea blue, Black, Grey, White");
+        questionAnswer.put("conversation-continue", "What else can I help you with?");
+        questionAnswer.put("conversation-complete", "Nice chatting with you. Bbye.");
 
-	public static void main(String[] args) throws FileNotFoundException, IOException, InterruptedException {
+    }
 
-		// Train categorizer model to the training data we created.
-		DoccatModel model = trainCategorizerModel();
+    public static void main(String[] args) throws IOException {
 
-		// Take chat inputs from console (user) in a loop.
-		Scanner scanner = new Scanner(System.in);
-		while (true) {
+        DoccatModel model = trainCategorizerModel();
 
-			// Get chat input from user.
-			System.out.println("##### You:");
-			String userInput = scanner.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
 
-			// Break users chat input into sentences using sentence detection.
-			String[] sentences = breakSentences(userInput);
+            System.out.println("##### You:");
+            String userInput = scanner.nextLine();
 
-			String answer = "";
-			boolean conversationComplete = false;
+//            String[] sentences = breakSentences(userInput);
+//
+//            String answer = "";
+//            boolean conversationComplete = false;
+//
+//            for (String sentence : sentences) {
+//
+//                String[] tokens = tokenizeSentence(sentence);
+//
+//                String[] posTags = detectPOSTags(tokens);
+//
+//                String[] lemmas = lemmatizeTokens(tokens, posTags);
+//
+//                String category = detectCategory(model, lemmas);
+//
+//                answer = answer + " " + questionAnswer.get(category);
+//
+//                if ("conversation-complete".equals(category)) {
+//                    conversationComplete = true;
+//                }
+            }
+//            TokenNameFinderEvaluator evaluator = new TokenNameFinderEvaluator(new NameFinderME(model));
+//            evaluator.evaluate(sampleStream);
 
-			// Loop through sentences.
-			for (String sentence : sentences) {
+//            FMeasure result = evaluator.getFMeasure();
 
-				// Separate words from each sentence using tokenizer.
-				String[] tokens = tokenizeSentence(sentence);
+//            System.out.println(result.toString());
+//            System.out.println("##### Chat Bot: " + answer);
+//            if (conversationComplete) {
+//                break;
+//            }
 
-				// Tag separated words with POS tags to understand their gramatical structure.
-				String[] posTags = detectPOSTags(tokens);
+//        }
 
-				// Lemmatize each word so that its easy to categorize.
-				String[] lemmas = lemmatizeTokens(tokens, posTags);
+    }
 
-				// Determine BEST category using lemmatized tokens used a mode that we trained
-				// at start.
-				String category = detectCategory(model, lemmas);
+    public static DoccatModel trainCategorizerModel() throws IOException {
+        InputStreamFactory inputStreamFactory = new MarkableFileInputStreamFactory(new File("faq-categorizer.txt"));
+        ObjectStream<String> lineStream = new PlainTextByLineStream(inputStreamFactory, StandardCharsets.UTF_8);
+        ObjectStream<DocumentSample> sampleStream = new DocumentSampleStream(lineStream);
 
-				// Get predefined answer from given category & add to answer.
-				answer = answer + " " + questionAnswer.get(category);
+        DoccatFactory factory = new DoccatFactory(new FeatureGenerator[]{new BagOfWordsFeatureGenerator()});
 
-				// If category conversation-complete, we will end chat conversation.
-				if ("conversation-complete".equals(category)) {
-					conversationComplete = true;
-				}
-			}
+        TrainingParameters params = ModelUtil.createDefaultTrainingParameters();
+        params.put(TrainingParameters.CUTOFF_PARAM, 0);
+        params.put(TrainingParameters.ITERATIONS_PARAM, 10);
 
-			// Print answer back to user. If conversation is marked as complete, then end
-			// loop & program.
-			System.out.println("##### Chat Bot: " + answer);
-			if (conversationComplete) {
-				break;
-			}
+        DoccatModel model = DocumentCategorizerME.train("en", sampleStream, TrainingParameters.defaultParams(), factory);
+        return model;
+    }
 
-		}
+    public String detectCategory(DoccatModel model, String[] finalTokens) {
 
-	}
+        DocumentCategorizerME myCategorizer = new DocumentCategorizerME(model);
 
-	/**
-	 * Train categorizer model as per the category sample training data we created.
-	 * 
-	 * @return
-	 * @throws FileNotFoundException
-	 * @throws IOException
-	 */
-	private static DoccatModel trainCategorizerModel() throws FileNotFoundException, IOException {
-		// faq-categorizer.txt is a custom training data with categories as per our chat
-		// requirements.
-		InputStreamFactory inputStreamFactory = new MarkableFileInputStreamFactory(new File("faq-categorizer.txt"));
-		ObjectStream<String> lineStream = new PlainTextByLineStream(inputStreamFactory, StandardCharsets.UTF_8);
-		ObjectStream<DocumentSample> sampleStream = new DocumentSampleStream(lineStream);
+        double[] probabilitiesOfOutcomes = myCategorizer.categorize(finalTokens,questionAnswers);
+        String category = myCategorizer.getBestCategory(probabilitiesOfOutcomes);
+        System.out.println("Category: " + category);
 
-		DoccatFactory factory = new DoccatFactory(new FeatureGenerator[] { new BagOfWordsFeatureGenerator() });
+        return category;
 
-		TrainingParameters params = ModelUtil.createDefaultTrainingParameters();
-		params.put(TrainingParameters.CUTOFF_PARAM, 0);
+    }
 
-		// Train a model with classifications from above file.
-		DoccatModel model = DocumentCategorizerME.train("en", sampleStream, params, factory);
-		return model;
-	}
+    public String[] breakSentences(String data) throws IOException {
+        try (InputStream modelIn = new FileInputStream("en-sent.bin")) {
 
-	/**
-	 * Detect category using given token. Use categorizer feature of Apache OpenNLP.
-	 * 
-	 * @param model
-	 * @param finalTokens
-	 * @return
-	 * @throws IOException
-	 */
-	private static String detectCategory(DoccatModel model, String[] finalTokens) throws IOException {
+            SentenceDetectorME myCategorizer = new SentenceDetectorME(new SentenceModel(modelIn));
 
-		// Initialize document categorizer tool
-		DocumentCategorizerME myCategorizer = new DocumentCategorizerME(model);
+            String[] sentences = myCategorizer.sentDetect(data);
+            System.out.println("Sentence Detection: " + Arrays.stream(sentences).collect(Collectors.joining(" | ")));
 
-		// Get best possible category.
-		double[] probabilitiesOfOutcomes = myCategorizer.categorize(finalTokens);
-		String category = myCategorizer.getBestCategory(probabilitiesOfOutcomes);
-		System.out.println("Category: " + category);
+            return sentences;
+        }
+    }
 
-		return category;
+    public String[] tokenizeSentence(String sentence) throws IOException {
+        try (InputStream modelIn = new FileInputStream("en-token.bin")) {
 
-	}
+            TokenizerME myCategorizer = new TokenizerME(new TokenizerModel(modelIn));
 
-	/**
-	 * Break data into sentences using sentence detection feature of Apache OpenNLP.
-	 * 
-	 * @param data
-	 * @return
-	 * @throws FileNotFoundException
-	 * @throws IOException
-	 */
-	private static String[] breakSentences(String data) throws FileNotFoundException, IOException {
-		// Better to read file once at start of program & store model in instance
-		// variable. but keeping here for simplicity in understanding.
-		try (InputStream modelIn = new FileInputStream("en-sent.bin")) {
+            String[] tokens = myCategorizer.tokenize(sentence);
+            System.out.println("Tokenizer : " + Arrays.stream(tokens).collect(Collectors.joining(" | ")));
 
-			SentenceDetectorME myCategorizer = new SentenceDetectorME(new SentenceModel(modelIn));
+            return tokens;
 
-			String[] sentences = myCategorizer.sentDetect(data);
-			System.out.println("Sentence Detection: " + Arrays.stream(sentences).collect(Collectors.joining(" | ")));
+        }
+    }
 
-			return sentences;
-		}
-	}
+    public String[] detectPOSTags(String[] tokens) throws IOException {
+        try (InputStream modelIn = new FileInputStream("en-pos-maxent.bin")) {
 
-	/**
-	 * Break sentence into words & punctuation marks using tokenizer feature of
-	 * Apache OpenNLP.
-	 * 
-	 * @param sentence
-	 * @return
-	 * @throws FileNotFoundException
-	 * @throws IOException
-	 */
-	private static String[] tokenizeSentence(String sentence) throws FileNotFoundException, IOException {
-		// Better to read file once at start of program & store model in instance
-		// variable. but keeping here for simplicity in understanding.
-		try (InputStream modelIn = new FileInputStream("en-token.bin")) {
+            POSTaggerME myCategorizer = new POSTaggerME(new POSModel(modelIn));
 
-			// Initialize tokenizer tool
-			TokenizerME myCategorizer = new TokenizerME(new TokenizerModel(modelIn));
+            String[] posTokens = myCategorizer.tag(tokens);
+            System.out.println("POS Tags : " + Arrays.stream(posTokens).collect(Collectors.joining(" | ")));
 
-			// Tokenize sentence.
-			String[] tokens = myCategorizer.tokenize(sentence);
-			System.out.println("Tokenizer : " + Arrays.stream(tokens).collect(Collectors.joining(" | ")));
+            return posTokens;
 
-			return tokens;
+        }
 
-		}
-	}
+    }
 
-	/**
-	 * Find part-of-speech or POS tags of all tokens using POS tagger feature of
-	 * Apache OpenNLP.
-	 * 
-	 * @param tokens
-	 * @return
-	 * @throws IOException
-	 */
-	private static String[] detectPOSTags(String[] tokens) throws IOException {
-		// Better to read file once at start of program & store model in instance
-		// variable. but keeping here for simplicity in understanding.
-		try (InputStream modelIn = new FileInputStream("en-pos-maxent.bin")) {
+    public String[] lemmatizeTokens(String[] tokens, String[] posTags)
+            throws IOException {
+        try (InputStream modelIn = new FileInputStream("en-lemmatizer.bin")) {
 
-			// Initialize POS tagger tool
-			POSTaggerME myCategorizer = new POSTaggerME(new POSModel(modelIn));
+            LemmatizerME myCategorizer = new LemmatizerME(new LemmatizerModel(modelIn));
+            String[] lemmaTokens = myCategorizer.lemmatize(tokens, posTags);
+            System.out.println("Lemmatizer : " + Arrays.stream(lemmaTokens).collect(Collectors.joining(" | ")));
 
-			// Tag sentence.
-			String[] posTokens = myCategorizer.tag(tokens);
-			System.out.println("POS Tags : " + Arrays.stream(posTokens).collect(Collectors.joining(" | ")));
+            return lemmaTokens;
 
-			return posTokens;
-
-		}
-
-	}
-
-	/**
-	 * Find lemma of tokens using lemmatizer feature of Apache OpenNLP.
-	 * 
-	 * @param tokens
-	 * @param posTags
-	 * @return
-	 * @throws InvalidFormatException
-	 * @throws IOException
-	 */
-	private static String[] lemmatizeTokens(String[] tokens, String[] posTags)
-			throws InvalidFormatException, IOException {
-		// Better to read file once at start of program & store model in instance
-		// variable. but keeping here for simplicity in understanding.
-		try (InputStream modelIn = new FileInputStream("en-lemmatizer.bin")) {
-
-			// Tag sentence.
-			LemmatizerME myCategorizer = new LemmatizerME(new LemmatizerModel(modelIn));
-			String[] lemmaTokens = myCategorizer.lemmatize(tokens, posTags);
-			System.out.println("Lemmatizer : " + Arrays.stream(lemmaTokens).collect(Collectors.joining(" | ")));
-
-			return lemmaTokens;
-
-		}
-	}
+        }
+    }
 
 }
---
+++ src/main/java/com/itsallbinary/namefinderBot/FindTeams.java
@@ -0,0 +1,177 @@
+package com.itsallbinary.namefinderBot;
+
+import com.itsallbinary.OpenNLPChatBot;
+import opennlp.tools.cmdline.SystemInputStreamFactory;
+import opennlp.tools.cmdline.doccat.DoccatEvaluatorTool;
+import opennlp.tools.doccat.*;
+import opennlp.tools.ml.maxent.GISTrainer;
+import opennlp.tools.namefind.*;
+import opennlp.tools.util.*;
+import opennlp.tools.util.eval.FMeasure;
+import opennlp.tools.util.model.ModelUtil;
+
+import javax.swing.event.DocumentListener;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import static opennlp.tools.ml.TrainerFactory.TrainerType.SEQUENCE_TRAINER;
+
+public class FindTeams {
+
+    private static Map<String, String> questionAnswer = new HashMap<>();
+    static OpenNLPChatBot openNLPChatBot = new OpenNLPChatBot();
+
+    static {
+        questionAnswer.put("greeting", "Hello, how can I help you?");
+        questionAnswer.put("team-names", "Stratus, NynemKonto, Emerging Tech & Innovation");
+//        questionAnswer.put("stratus-enquiry", "Mamatha, Srinivas, Henrik, Mads, Shilpa");
+        questionAnswer.put("nynemkonto-enquiry", "Mamatha, Srinivas, Henrik, Mads, Shilpa, Shaista, Pranati, Nithya, Karthik, Shrinivasa");
+        questionAnswer.put("emergingTech-enquiry", "Mamatha, Shaista, Shrusti, Shilpa, Karthik, Shrinivasa");
+        questionAnswer.put("team-experience", "5");
+        questionAnswer.put("conversation-continue", "What else can I help you with?");
+        questionAnswer.put("conversation-complete", "Nice chatting with you. Bbye.");
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        InputStreamFactory isf = () -> new FileInputStream("team-namefinder.txt");
+        Charset charset = Charset.forName("UTF-8");
+        ObjectStream<String> lineStream = new PlainTextByLineStream(isf, charset);
+        ObjectStream<NameSample> sampleStream = new NameSampleDataStream(lineStream);
+
+        TokenNameFinderModel model;
+        TokenNameFinderFactory nameFinderFactory = new TokenNameFinderFactory();
+
+        try {
+            model = NameFinderME.train("en", null, sampleStream, TrainingParameters.defaultParams(),
+                    nameFinderFactory);
+        } finally {
+            sampleStream.close();
+        }
+
+        BufferedOutputStream modelOut = null;
+
+        try {
+            modelOut = new BufferedOutputStream(new FileOutputStream("ner-custom-model.bin"));
+            model.serialize(modelOut);
+        } finally {
+            if (modelOut != null)
+                modelOut.close();
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+
+            System.out.println("##### You:");
+            String userInput = scanner.nextLine();
+
+            String[] sentences = openNLPChatBot.breakSentences(userInput);
+
+            String answer = "";
+            boolean conversationComplete = false;
+            for (String sentence : sentences) {
+
+                String[] tokens = openNLPChatBot.tokenizeSentence(sentence);
+
+                String[] posTags = openNLPChatBot.detectPOSTags(tokens);
+
+                String[] lemmas = openNLPChatBot.lemmatizeTokens(tokens, posTags);
+                DoccatModel categorizeModel = openNLPChatBot.trainCategorizerModel();
+
+                String category = openNLPChatBot.detectCategory(categorizeModel, lemmas);
+
+                answer = answer + " " + questionAnswer.get(category);
+
+                if ("conversation-complete".equals(category)) {
+                    conversationComplete = true;
+                }
+            }
+//            TokenNameFinderEvaluator evaluator = new TokenNameFinderEvaluator(new NameFinderME(model));
+//            evaluator.evaluate(sampleStream);
+
+//            FMeasure result = evaluator.getFMeasure();
+
+//            System.out.println(result.toString());
+            System.out.println("##### Chat Bot: " + answer);
+            if (conversationComplete) {
+                break;
+            }
+        }
+
+    }
+
+//        TokenNameFinderModel model = trainNameFinderModel();
+//        System.out.println(Arrays.toString(model.getNameFinderSequenceModel().getOutcomes()));
+//        nameFinder();
+//        Scanner scanner = new Scanner(System.in);
+//        while (true) {
+//
+//            System.out.println("##### You:");
+//            String userInput = scanner.nextLine();
+//
+//            String[] sentences = openNLPChatBot.breakSentences(userInput);
+//
+//            String answer = "";
+//            boolean conversationComplete = false;
+
+//            for (String sentence : sentences) {
+//
+//                String[] tokens = openNLPChatBot.tokenizeSentence(sentence);
+//
+//                String[] posTags = openNLPChatBot.detectPOSTags(tokens);
+//
+//                String[] lemmas = openNLPChatBot.lemmatizeTokens(tokens, posTags);
+//
+//                String category = openNLPChatBot.detectCategory(model, lemmas);
+//
+//                answer = answer + " " + questionAnswer.get(category);
+//
+//                if ("conversation-complete".equals(category)) {
+//                    conversationComplete = true;
+//                }
+//            }
+////            TokenNameFinderEvaluator evaluator = new TokenNameFinderEvaluator(new NameFinderME(model));
+////            evaluator.evaluate(sampleStream);
+//
+////            FMeasure result = evaluator.getFMeasure();
+//
+////            System.out.println(result.toString());
+//            System.out.println("##### Chat Bot: " + answer);
+//            if (conversationComplete) {
+//                break;
+//            }
+
+        }
+
+//    public static void nameFinder() throws IOException {
+//        TokenNameFinder nameFinder = new NameFinderME(trainNameFinderModel());
+//        InputStreamFactory sampleDataIn = (InputStreamFactory) new FileInputStream("en-ner-person.bin");
+//        ObjectStream sampleStream = new PlainTextByLineStream(sampleDataIn, StandardCharsets.UTF_8);
+//        TokenNameFinderEvaluator evaluator = new TokenNameFinderEvaluator(nameFinder);
+//        evaluator.evaluate(sampleStream);
+//
+//        FMeasure result = evaluator.getFMeasure();
+//
+//        System.out.println(result.toString());
+//    }
+//
+//    public static TokenNameFinderModel trainNameFinderModel() throws IOException {
+//        InputStreamFactory inputStreamFactory = () -> new FileInputStream("team-namefinder.txt");
+//        ObjectStream<String> lineStream = new PlainTextByLineStream(inputStreamFactory, StandardCharsets.UTF_8);
+//        ObjectStream<NameSample> sampleStream = new NameSampleDataStream(lineStream);
+//
+//        TokenNameFinderFactory factory = new TokenNameFinderFactory();
+//
+//        TrainingParameters params = ModelUtil.createDefaultTrainingParameters();
+//        params.put(TrainingParameters.CUTOFF_PARAM, 1);
+//        params.put(TrainingParameters.TRAINER_TYPE_PARAM, String.valueOf(SEQUENCE_TRAINER));
+//
+//        TokenNameFinderModel model = NameFinderME.train("en", null,sampleStream, params, factory);
+//        return model;
+//    }
